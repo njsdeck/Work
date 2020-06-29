d6ac7a064c9096144b8d7cd24a354be62d391eab
njsdeck <46569118+njsdeck@users.noreply.github.com>
2020-06-29 10:48:17 -0400

    Pushing repeat detector
    Reads a txt file to see how many words repeat in the file. Prints the total percentage of the file that is repeated.

---
+++ Repeat detector/repeat.py
@@ -0,0 +1,24 @@
+new=[]
+def main():
+    filename = input("Please type the name of the text file you wish to open. Please include the .txt.")
+    total,repeat = count(filename)
+    print(total)
+    print(repeat)
+    percentage(total,repeat)
+def count(filename):
+    total=0
+    repeat=0
+    with open(filename,'r') as f:
+        for line in f:
+            for word in line.split():
+                if new.count(word) > 0:
+                    repeat+=1
+                else:
+                    new.append(word)
+                total+=1
+    return total,repeat
+def percentage(total,repeat):
+    percent = repeat/total
+    percent = percent*100
+    print(percent)
+main()
