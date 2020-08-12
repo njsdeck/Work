da97d92caf924da2c9ff5839d74e98eee03a431a
njsdeck <46569118+njsdeck@users.noreply.github.com>
2020-08-12 13:32:30 -0400

    Added partial ability check rolls and the ability to create rolled stats

--- Character/character.py
+++ Character/character.py
@@ -8,3 +8,116 @@ class character:
         self.char=char
         self.wis=wis
         self.int=int
+    def dex_check(self):
+        '''
+        Does the dexterity check.
+        :return: None
+        '''
+        check=random.randint(1,20)+self.dex #Rolls the dexterity roll
+        print(check)
+        if check in range(13,18): #This is the realm of the roll succeeding.
+            print("You made the check and continue on your way")
+        if check in range(18,20): #Tbe roll went really well.
+            print("You are seriously dexterous in the moment.")
+        if check>=20: # This roll is the phenomenal role.
+            print("You were doing cartwheels as you did this")
+        #if check<13: # roll fails.
+            # damage=random.randint(1,4)+random.randint(1,6)+random.randint(1,6) #rolls 3 dice and adds them together.
+            # print("You fail the action and take " +str(damage)+ " damage to your health")
+            # self.health=self.health-damage # subtracts the damage.
+            # print("Health:" +str(self.health))
+    def str_check(self):
+        '''
+        Does the strength roll
+        :return: None
+        '''
+        check=random.randint(1,20)+self.str
+        print(check)
+        if check in range(13,18):
+            print("You made the check and continue on your way")
+        if check in range(18,20):
+            print("You are seriously muscular in the moment.")
+        if check>=(20):
+            print("You were doing pushup and flexing the whole time you did this")
+        # if check<13:
+        #     damage=random.randint(1,4)+random.randint(1,6)+random.randint(1,6)
+        #     print("You fail the action and take " +str(damage)+ " damage to your health")
+        #     self.health=self.health-damage
+        #     print("Health:" +str(self.health))
+    def con_check(self):
+        '''
+        Does the constitution check.
+        :return: none
+        '''
+        check=random.randint(1,20)+self.con
+        print(check)
+        if check in range(13,18):
+            print("You made the check and continue on your way")
+        if check in range(18,20):
+            print("You had a stomach of steel")
+        if check>=20:
+            print("That was the strangest thing you have ever tasted. You gain extra health")
+            gain=random.randint(1,6)
+            print("You gained " +str(gain)+ " health.")
+            self.health+=gain
+        # if check<13:
+        #     damage=random.randint(1,4)+random.randint(1,6)+random.randint(1,6)
+        #     print("You fail the action and take " +str(damage)+ " damage to your health")
+        #     self.health=self.health-damage
+        #     print("Health:" +str(self.health))
+    def char_check(self):
+        '''
+        Does the charisma check.
+        :return: None
+        '''
+        check=random.randint(1,20)+self.char
+        print(check)
+        if check in range(13,18):
+            print("You made the check and continue on your way")
+        if check in range(18,20):
+            print("You laid the cute animal eyes on thick")
+        if check>=20:
+            print("You were so smooth they even fed you")
+            self.health=self.health+1
+            print("Health:" +str(self.health))
+        # if check<13:
+        #     damage=random.randint(1,4)+random.randint(1,6)+random.randint(1,6)
+        #     print("They hit you and you take " +str(damage)+ " damage to your health. You run by")
+        #     self.health=self.health-damage
+        #     print("Health:" +str(self.health))
+    def wis_check(self):
+        '''
+        Does the wisdom check
+        :return: None
+        '''
+        check=random.randint(1,20)+self.wis
+        print(check)
+        if check in range(13,18):
+            print("You made the check and continue on your way")
+        if check in range(18,20):
+            print("Your wise thoughts made the challenge a breeze.")
+        if check>=20:
+            print("You were the wisest you have ever been as you did this")
+        # if check<13:
+        #     damage=random.randint(1,4)+random.randint(1,6)+random.randint(1,6)
+        #     print("You fail the action and take " +str(damage)+ " emotional damage to your health")
+        #     self.health=self.health-damage
+        #     print("Health:" +str(self.health))
+    def int_check(self):
+        '''
+        Does the intelligence check.
+        :return: None
+        '''
+        check=random.randint(1,20)+self.int
+        print(check)
+        if check in range(13,18):
+            print("You made the check and continue on your way")
+        if check in range(18,20):
+            print("You easily figured out the answer in the moment.")
+        if check>=20:
+            print("You did so many smart things it would be impossible to remember it")
+        # if check<13:
+        #     damage=random.randint(1,4)+random.randint(1,6)+random.randint(1,6)
+        #     print("You fail the action and take " +str(damage)+ " brain damage to your health")
+        #     self.health=self.health-damage
+        #     print("Health:" +str(self.health))
--- Character/primary.py
+++ Character/primary.py
@@ -1,3 +1,4 @@
+import random
 from character import character
 def main():
     choice1 = int(input("Press 1 for half-orc, 2 for human, 3 for tiefling, 4 for halfling, 5 for dragonborn, or 6 for assimar."))
@@ -7,15 +8,40 @@ def main():
 def ability_score(race):
     power=input("Press 1 to use a stat array. 2 to roll stats.")
     if power == "1":
-        array = input("What array would you like to use? Type 1 for 5 10's and a 16, 2 for all 12's, or 3 for 4 10's and  14's?")
+        array = input("What array would you like to use? Type 1 for 5 10's and a 16, 2 for all 12's, or 3 for 4 10's and 2 14's?")
         if array == "1":
             list = [10,10,10,10,10,16]
             list,cscore=charisma(list)
             list,iscore=intel(list)
             list,wscore=wisdom(list)
+            list,sscore=strength(list)
+            list,dscore=dexterity(list)
+            list,conscore=constitution(list)
+            Character = character(race,sscore,dscore,conscore,cscore,wscore,iscore)
         if array == "2":
             Character = character(race,10,10,10,10,10,10)
             print("You made it.")
+        if array == "3":
+            list = [10,10,10,10,14,14]
+            list,cscore=charisma(list)
+            list,iscore=intel(list)
+            list,wscore=wisdom(list)
+            list,sscore=strength(list)
+            list,dscore=dexterity(list)
+            list,conscore=constitution(list)
+            Character = character(race,sscore,dscore,conscore,cscore,wscore,iscore)
+    if power == "2":
+        check=[]
+        for i in range(6):
+            check.append(roll())
+        print(check)
+        check,cscore=charisma(check)
+        check,iscore=intel(check)
+        check,wscore=wisdom(check)
+        check,sscore=strength(check)
+        check,dscore=dexterity(check)
+        check,conscore=constitution(check)
+        Character = character(race,sscore,dscore,conscore,cscore,wscore,iscore)
 def race_creation(i):
     choice2 ={
     1:"Half-orc",
@@ -28,23 +54,26 @@ def race_creation(i):
     return choice2.get(i,"Wrong Answer")
 def charisma(list):
     counter=0
-    charisma = int(input("What would you like your charisma score to be?"))
-    print("You have the following ability scores left.")
+    char = int(input("What would you like your charisma score to be?"))
+    # if char not in list:
+    #     print("Not a valid input. Try again.")
+    #     charisma(list)
     for i in list:
-        if i == charisma:
+        if i == char:
             list.pop(counter)
+            print("You have the following ability scores left.")
             break
-        if i != charisma:
+        if i != char:
             counter+=1
     print(list)
     return list,charisma
 def intel(list):
     counter=0
     intel = int(input("What would you like your intelligence score to be?"))
-    print("You have the following ability scores left.")
     for i in list:
         if i == intel:
             list.pop(counter)
+            print("You have the following ability scores left.")
             break
         if i != intel:
             counter+=1
@@ -53,13 +82,63 @@ def intel(list):
 def wisdom(list):
     counter=0
     wisdom = int(input("What would you like your wisdom score to be?"))
-    print("You have the following ability scores left.")
     for i in list:
         if i == wisdom:
             list.pop(counter)
+            print("You have the following ability scores left.")
             break
         if i != wisdom:
             counter+=1
     print(list)
     return list,wisdom
+def strength(list):
+    counter=0
+    strength = int(input("What would you like your strength score to be?"))
+    for i in list:
+        if i == strength:
+            list.pop(counter)
+            counter=0
+            print("You have the following ability scores left.")
+            break
+        if i != strength:
+            counter+=1
+    print(list)
+    return list,strength
+def dexterity(list):
+    counter=0
+    dex = int(input("What would you like your dexterity score to be?"))
+    for i in list:
+        if i == dex:
+            list.pop(counter)
+            counter=0
+            print("You have the following ability scores left.")
+            break
+        if i != dex:
+            counter+=1
+    print(list)
+    return list,dex
+def constitution(list):
+    counter=0
+    con = int(input("What would you like your constitution score to be?"))
+    for i in list:
+        if i == con:
+            list.pop(counter)
+            counter=0
+            print("You have the following ability scores left.")
+            break
+        if i != con:
+            counter+=1
+    return list,con
+def roll():
+    rolled=0
+    new_roll=[]
+    for i in range(4):
+        i=random.randint(1,6)
+        new_roll.append(i)
+    new_roll.sort()
+    new_roll.pop(0)
+    for i in new_roll:
+        rolled=rolled+i
+    print(rolled)
+    return rolled
 main()
