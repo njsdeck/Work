6e502da9e020c5aea70dd15024b7c60be462334d
njsdeck <46569118+njsdeck@users.noreply.github.com>
2021-05-09 08:26:14 -0400

    Fixed the ability to exit out early with a keypress.

--- alarm.py
+++ alarm.py
@@ -1,15 +1,16 @@
 from playsound import playsound
 import time
+import keyboard
 def main():
-    # testing = True
     alarm=input("What time would you like the alarm to play. Put in H:M:S")
-    # while testing == True:
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
     while current_time != alarm:
         t = time.localtime()
         current_time = time.strftime("%H:%M:%S", t)
+        if keyboard.is_pressed("`") == True:
+            quit()
+        else:
+            pass
     playsound("alarm.wav")
-    # testing=False
-        #print(current_time)
 main()
